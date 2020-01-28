088815ecbfa4b4c262223533fb4a66a7c133c790
unknown <sinnehae@gmail.com>
2020-01-28 11:10:00 +0900

    create to ex1.js

---
+++ ex1.js
@@ -0,0 +1,14 @@
+var txt1 = "text1";
+console.log(txt1);
+
+if (true) {
+  var txt2 = "text2";
+  console.log(txt2);
+}
+
+let txt3 = "text3"; //같은 블록 안에서만 통용
+const txt4 = "kim"; //변경 불가능
+console.log(txt3);
+console.log(txt4);
+txt3 = "lee";
+console.log(txt3);
